47d0192ea969e209055ba98243c74740cf40223a
MauricioLedber <drakpreepers1234@gmail.com>
2024-10-22 23:49:44 -0600

    Add files via upload

---
+++ Rpg/src/Gui/MainPanel.java
@@ -0,0 +1,66 @@
+package gui.panels;
+
+import enemies.Enemy;
+import gui.labels.PlayerSpriteLabel;
+import player.Player;
+import gui.labels.SpriteLabel;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+/**
+ * Clase que crea el panel principal.
+ */
+public class MainPanel extends BackGroundPanel {
+
+	private static MainPanel instance;
+	private Enemy enemy;
+	private final Player player;
+	private JPanel mainPanel;
+	private JPanel spritesPanel;
+	private JPanel dialogPanel;
+	private JLabel enemySprite;
+	private JLabel playerSprite;
+	private JPanel playerSpritePanel;
+	private JPanel enemySpritePanel;
+
+	public static MainPanel getInstance(Enemy enemy, Player player) {
+
+		if (instance == null)
+			instance = new MainPanel(enemy, player);
+		return instance;
+	}
+	/**
+	 * Constructor de la clase
+
+	 */
+	private MainPanel(Enemy enemy, Player player) {
+
+		super();
+		this.enemy = enemy;
+		this.player = player;
+		add(mainPanel);
+		Dimension spritePanelSize = new Dimension(getPreferredSize().width / 2, getPreferredSize().height/2);
+		playerSpritePanel.setPreferredSize(spritePanelSize);
+		enemySpritePanel.setPreferredSize(spritePanelSize);
+	}
+
+	public void update(Enemy enemy) {
+
+		((SpriteLabel) enemySprite).updateImage(enemy.getImage());
+		repaint();
+	}
+
+	private void createUIComponents() {
+
+		dialogPanel = DialogPanel.getInstance();
+		playerSprite = new PlayerSpriteLabel(player.getImage());
+		enemySprite = new SpriteLabel(enemy.getImage());
+	}
+
+	public void setEnemy(Enemy enemy) {
+
+		this.enemy = enemy;
+	}
+}
