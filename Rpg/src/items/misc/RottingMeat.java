d5d2078c097ec54e2ef6fb8f7e684432fe6ee1fb
MauricioLedber <drakpreepers1234@gmail.com>
2024-10-31 09:56:10 -0600

    Delete Rpg/src directory

--- Rpg/src/items/misc/RottingMeat.java
+++
@@ -1,9 +0,0 @@
-package items.misc;
-import items.Item;
-import java.io.Serializable;
-
-public class RottingMeat extends Item implements Serializable {
-    public RottingMeat(){
-        super("Carne en descomposición", "Estó no se puede comer!!", 0);
-    }
-}
--- Rpg/src/player/debuffs/DebuffType.java
+++
@@ -1,7 +0,0 @@
-package player.debuffs;
-/**
- *La clase DebuffType define un conjunto de constantes que representan diferentes tipos de efectos negativos.
- * */
-public enum DebuffType {
-    WEAKNESS, POISON, BLEED, BURN, STUN, SLOW, FROZEN, SLEEP, CONFUSION, FEAR, BLIND, SILENCE, CURSE
-}
